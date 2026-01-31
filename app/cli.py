99dccc6ed786fc7bc55ada34d0a598524ffdc8df
Kadian Ramkissoon <83883407+KadianR@users.noreply.github.com>
2026-01-31 11:27:21 -0400

    Finished exercises

--- app/cli.py
+++ app/cli.py
@@ -1,3 +1,4 @@
+from typing_extensions import Annotated
 import typer
 from app.database import create_db_and_tables, get_session, drop_all
 from app.models import User
@@ -7,7 +8,7 @@ from sqlalchemy.exc import IntegrityError
 
 cli = typer.Typer()
 
-@cli.command()
+@cli.command(help = "Initializes the database and creates a default user 'bob")
 def initialize():
     with get_session() as db: # Get a connection to the database
         drop_all() # delete all tables
@@ -18,8 +19,8 @@ def initialize():
         db.refresh(bob) # Update the user (we use this to get the ID from the db)
         print("Database Initialized")
 
-@cli.command()
-def get_user(username:str):
+@cli.command(help = "Get a user by their username")
+def get_user(username:Annotated[str, typer.Argument(help = "The username of the user to retrieve")]):
     with get_session() as db: # Get a connection to the database
         user = db.exec(select(User).where(User.username == username)).first()
         if not user:
@@ -27,7 +28,7 @@ def get_user(username:str):
             return
         print(user)
 
-@cli.command()
+@cli.command(help = "Retrieves and prints all users from the database")
 def get_all_users():
     with get_session() as db:
         all_users = db.exec(select(User)).all()
@@ -38,8 +39,9 @@ def get_all_users():
                 print(user)
 
 
-@cli.command()
-def change_email(username: str, new_email:str):
+@cli.command(help = "Updates a user's email by the given username")
+def change_email(username: Annotated[str, typer.Argument(help = "The username of the user whose email is to be updated")],                 
+                 new_email: Annotated[str, typer.Argument(help = "The new email address to update for the user")]):
     with get_session() as db: # Get a connection to the database
         user = db.exec(select(User).where(User.username == username)).first()
         if not user:
@@ -50,8 +52,10 @@ def change_email(username: str, new_email:str):
         db.commit()
         print(f"Updated {user.username}'s email to {user.email}")
 
-@cli.command()
-def create_user(username: str, email:str, password: str):
+@cli.command(help = "Creates a new user with the given username, email, and password and adds details to the database")
+def create_user(username: Annotated[str, typer.Argument(help = "The username of the new user to be created")], 
+                email: Annotated[str, typer.Argument(help = "The email address of the new user to be created")], 
+                password: Annotated[str, typer.Argument(help = "The password of the new user to be created")]):
     with get_session() as db: # Get a connection to the database
         newuser = User(username, email, password)
         try:
@@ -64,8 +68,8 @@ def create_user(username: str, email:str, password: str):
         else:
             print(newuser) # print the newly created user
 
-@cli.command()
-def delete_user(username: str):
+@cli.command(help = "Deletes a user by the given username from the database")
+def delete_user(username: Annotated[str, typer.Argument(help = "The username of the user to be deleted from the database")]):
     with get_session() as db:
         user = db.exec(select(User).where(User.username == username)).first()
         if not user:
@@ -76,8 +80,8 @@ def delete_user(username: str):
         print(f'{username} deleted')
 
 #Create a cli command that allows you to find a user using a partial match of their email OR username.
-@cli.command()
-def find_user(query: str):
+@cli.command(help = "Finds and prints users whose username or email contains the given partial string")
+def find_user(query: Annotated[str, typer.Argument(help = "The partial string to search for in usernames and emails")]):
     with get_session() as db:
         users = db.exec(select(User).where((User.username.contains(query)) | (User.email.contains(query)))).all()
         if not users:
@@ -87,8 +91,9 @@ def find_user(query: str):
             print(user)
 
 #Create cli command that allows you to list the first N users of the database to be used by a paginated table. The command should accept 2 arguments limit and offset and return the appropriate result. limit should be defaulted to 10 and offset should be defaulted to 0
-@cli.command()
-def list_users(limit: int = 10, offset: int = 0):
+@cli.command(help = "Lists users from the database by a paginated table using limit and offset values")
+def list_users(limit: Annotated[int, typer.Option(help = "The number of users to list (default 10)")] = 10, 
+               offset: Annotated[int, typer.Option(help = "The offset from the beginning of the list (default 0)")] = 0):
     with get_session() as db:
         users = db.exec(select(User).offset(offset).limit(limit)).all()
         if not users:
